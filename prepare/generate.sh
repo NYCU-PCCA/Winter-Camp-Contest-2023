7b533631cf88726ba450a6d7f1b1e8ab46567557
Allen Guan <me@nella17.tw>
2023-02-02 00:35:19 +0800

    fix prepare script

--- prepare/generate.sh
+++ prepare/generate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 export XDG_CONFIG_HOME=$PWD
+# TODO: p2d https://github.com/cn-xcpc-tools/testlib-for-domjudge/tree/master/p2d
+# https://github.com/cubercsl/polygon2domjudge
 p2d.sh problems
 mkdir gen
 mv *.zip gen
